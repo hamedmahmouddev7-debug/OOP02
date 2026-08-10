112e5289f82a6e9c622f5f709c1ff4cf5562a85e
hamedmahmouddev7-debug <hamedmahmoud.dev7@gmail.com>
2026-08-11 02:22:57 +0300

    Question02

--- OOPAssignment02/Program.cs
+++ OOPAssignment02/Program.cs
@@ -9,6 +9,16 @@
 
             // (B) Classes support inheritance and code reuse, so they are easier to manage.
             #endregion
+
+            #region Question02
+            // (A) Shipment is the parent class.
+
+            // (B) ExpressShipment is the child class because it inherits from Shipment 
+
+            // (C) ExpressShipment inherits the TrackingCode property from Shipment.
+
+            // (D) Inheritance allows code reuse and reduces code duplication, making programs easier to maintain and extend.
+            #endregion
         }
     }
 }
