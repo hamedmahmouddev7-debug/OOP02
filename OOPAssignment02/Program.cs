17e6c942322ffa8a76ca8a3110390299a80468a1
hamedmahmouddev7-debug <hamedmahmoud.dev7@gmail.com>
2026-08-11 02:19:54 +0300

    Question01

--- OOPAssignment02/Program.cs
+++ OOPAssignment02/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
+            #region Question01
+            // (A) A class is a reference type, while a struct is a value type.
 
+            // (B) Classes support inheritance and code reuse, so they are easier to manage.
+            #endregion
         }
     }
 }
