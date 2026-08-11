07161a26062a47938b97a9cd3a50d26d7186b0bd
hamedmahmouddev7-debug <hamedmahmoud.dev7@gmail.com>
2026-08-11 07:10:57 +0300

    Question03

--- OOPAssignment02/Program.cs
+++ OOPAssignment02/Program.cs
@@ -19,6 +19,135 @@
 
             // (D) Inheritance allows code reuse and reduces code duplication, making programs easier to maintain and extend.
             #endregion
+
+            #region Question03
+
+            //Console.Write("Enter Center Name: ");
+            //string centerName = Console.ReadLine();
+
+            //DeliveryCenter center = new DeliveryCenter(centerName);
+
+            //Console.WriteLine("\n--- Standard Shipment ---");
+
+            //Console.Write("Tracking Code: ");
+            //string trackingCode1 = Console.ReadLine();
+
+            //Console.Write("Description: ");
+            //string description1 = Console.ReadLine();
+
+            //Console.Write("Weight: ");
+            //decimal weight1 = decimal.Parse(Console.ReadLine());
+
+            //Console.Write("Delivery Fee: ");
+            //decimal deliveryFee1 = decimal.Parse(Console.ReadLine());
+
+            //Console.Write("City: ");
+            //string city1 = Console.ReadLine();
+
+            //Console.Write("Street: ");
+            //string street1 = Console.ReadLine();
+
+            //Console.Write("Building Number: ");
+            //int buildingNumber1 = int.Parse(Console.ReadLine());
+
+            //DeliveryAddress address1 =
+            //    new DeliveryAddress(city1, street1, buildingNumber1);
+
+            //StandardShipment standardShipment =
+            //    new StandardShipment(
+            //        trackingCode1,
+            //        description1,
+            //        weight1,
+            //        deliveryFee1,
+            //        address1
+            //    );
+
+            //center.AddShipment(standardShipment);
+
+
+            //Console.WriteLine("\n--- Express Shipment ---");
+
+            //Console.Write("Tracking Code: ");
+            //string trackingCode2 = Console.ReadLine();
+
+            //Console.Write("Description: ");
+            //string description2 = Console.ReadLine();
+
+            //Console.Write("Weight: ");
+            //decimal weight2 = decimal.Parse(Console.ReadLine());
+
+            //Console.Write("Delivery Fee: ");
+            //decimal deliveryFee2 = decimal.Parse(Console.ReadLine());
+
+            //Console.Write("City: ");
+            //string city2 = Console.ReadLine();
+
+            //Console.Write("Street: ");
+            //string street2 = Console.ReadLine();
+
+            //Console.Write("Building Number: ");
+            //int buildingNumber2 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Extra Fee: ");
+            //decimal extraFee = decimal.Parse(Console.ReadLine());
+
+            //DeliveryAddress address2 =
+            //    new DeliveryAddress(city2, street2, buildingNumber2);
+
+            //ExpressShipment expressShipment =
+            //    new ExpressShipment(
+            //        trackingCode2,
+            //        description2,
+            //        weight2,
+            //        deliveryFee2,
+            //        address2,
+            //        extraFee
+            //    );
+
+            //center.AddShipment(expressShipment); Console.WriteLine("\n--- Express Shipment ---");
+
+            //Console.Write("Tracking Code: ");
+            //string trackingCode2 = Console.ReadLine();
+
+            //Console.Write("Description: ");
+            //string description2 = Console.ReadLine();
+
+            //Console.Write("Weight: ");
+            //decimal weight2 = decimal.Parse(Console.ReadLine());
+
+            //Console.Write("Delivery Fee: ");
+            //decimal deliveryFee2 = decimal.Parse(Console.ReadLine());
+
+            //Console.Write("City: ");
+            //string city2 = Console.ReadLine();
+
+            //Console.Write("Street: ");
+            //string street2 = Console.ReadLine();
+
+            //Console.Write("Building Number: ");
+            //int buildingNumber2 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Extra Fee: ");
+            //decimal extraFee = decimal.Parse(Console.ReadLine());
+
+            //DeliveryAddress address2 =
+            //    new DeliveryAddress(city2, street2, buildingNumber2);
+
+            //ExpressShipment expressShipment =
+            //    new ExpressShipment(
+            //        trackingCode2,
+            //        description2,
+            //        weight2,
+            //        deliveryFee2,
+            //        address2,
+            //        extraFee
+            //    );
+
+            //center.AddShipment(expressShipment);
+
+
+
+            #endregion
         }
     }
 }
---
+++ OOPAssignment02/StandardShipment .cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAssignment02
+{
+    public class StandardShipment : Shipment
+    {
+       // public StandardShipment(
+       //string trackingCode,
+       //string description,
+       //decimal weight,
+       //decimal deliveryFee,
+       //DeliveryAddress destination) : base(trackingCode, description, weight, deliveryFee, destination)
+       // {
+       // }
+    }
+}
